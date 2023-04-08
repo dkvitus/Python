042b95b6322aad253bc88fb66c68877289e648ae
dkvitus <dkvitus@gmail.com>
2023-04-08 16:42:41 -0400

    Saved

---
+++ Day3/01_Conditions.py
@@ -0,0 +1,16 @@
+a = 5
+b = 6
+c = 5
+
+this_is_true = True
+this_is_false= not(this_is_true)
+
+print("True: ",this_is_true)
+print("False: ",this_is_false)
+
+
+
+# is_same = not(b)
+
+# print("Is a the same as b?", a==b)
+# print("Is a NOT the same as b?", a = not(b))
---
+++ Day3/02_if_else.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: \n"))
+b = 20
+
+if a > b :
+    print(a,"is larger than", b)
+else:
+    print(a,"is smaller than", b)
